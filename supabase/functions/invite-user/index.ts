cbf3acb7e1580f0486c47b9abd693d0f528dc5e4
jedmcdaniel253 <jedmcdaniel@gmail.com>
2026-04-20 15:51:42 -0400

    chore: update base path and URLs for custom domain auntiescleaners.com
    - Change base href from /AuntiesCleaners/ to /
    - Update service worker base path
    - Update invite-user redirect URL to auntiescleaners.com
    - Update bookmark URL in SetPassword page
    - Bump version to 1.0.12

--- supabase/functions/invite-user/index.ts
+++ supabase/functions/invite-user/index.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const REDIRECT_URL =
-  "https://jedmcdaniel253.github.io/AuntiesCleaners/set-password";
+  "https://auntiescleaners.com/set-password";
 
 export function buildInviteEmailHtml(
   name: string,
